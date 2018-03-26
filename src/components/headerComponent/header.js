7f8da759d5b78850498af080a0921be934b0022c
Jae Moon <vicbizmoon@hotmail.com>
2018-03-25 20:46:50 -0400

    TEST App using React and Gulp

---
+++ src/components/headerComponent/header.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import {
+    Link
+  } from 'react-router-dom';
+  
+class Header extends Component {
+  render() {
+    return (
+        <header>
+            <div className="logo">
+                LOGO
+            </div>
+            <nav>
+                <ul>
+                    <li className="first"><Link to="/">Home</Link></li>
+                    <li><Link to="/products">Products</Link></li>
+                    <li className="last"><Link to="/contact">Contact</Link></li>
+                </ul>
+            </nav>
+        </header>
+    );
+  }
+}
+
+export default Header;
